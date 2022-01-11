e52902b1ab784a8a5dc815b10c8a645de8b749b5
afzaanhakim <afzaanhakim19@gmail.com>
2022-01-10 21:56:08 -0500

    max screensize

--- components/Results.js
+++ components/Results.js
@@ -6,7 +6,7 @@ const Results = ({results}) => {
     <Thumbnail key={result.id} result={result}/>
   ))
   return (
-    <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 '>
+    <div className='px-5 my-10 sm:grid md:grid-cols-2 xl:grid-cols-3 3xl:flex flex-wrap justify-center'>
       
       {resultItem}
     </div>
