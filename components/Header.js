2a8da8eaff9dc11874752be40898bc130ecc2a27
afzaanhakim <afzaanhakim19@gmail.com>
2022-01-10 19:40:21 -0500

    header touches and design

--- components/Header.js
+++ components/Header.js
@@ -2,19 +2,33 @@
 import Image from "next/image";
 import Vidstar from "../assets/Vidstar.png"
 import HeaderItem from "./HeaderItem";
-import { BeakerIcon, HomeIcon, BadgeCheckIcon, LightningBoltIcon, UserIcon, CollectionIcon, LightBulbIcon, SearchIcon, UsersIcon } from '@heroicons/react/outline'
+import {
+  BeakerIcon,
+  HomeIcon,
+  BadgeCheckIcon,
+  LightningBoltIcon,
+  UserIcon,
+  CollectionIcon,
+  LightBulbIcon,
+  SearchIcon,
+  UsersIcon,
+} from "@heroicons/react/outline";
 
 const Header = () => {
   return (
-    <header className="">
-      <div>
-        <HeaderItem title='home' Icon={HomeIcon}/>
-        </div>
+    <header className="flex flex-col sm:flex-row mr-5 ml-5 mt-1  justify-between items-center h-auto">
+      <div className="flex flex-grow justify-evely max-w-2xl">
+        <HeaderItem title="HOME" Icon={HomeIcon} />
+        <HeaderItem title="TRENDING" Icon={LightningBoltIcon} />
+        <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
+        <HeaderItem title="SEARCH" Icon={SearchIcon} />
+        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+      </div>
       <Image
         className="object-contain"
         src={Vidstar}
-        width={400}
-        height={400}
+        width={200}
+        height={200}
         padding={0}
       />
     </header>
--- components/HeaderItem.js
+++ components/HeaderItem.js
@@ -1,9 +1,12 @@
 
 
-const HeaderItem = ({...props}) => {
+const HeaderItem = (props) => {
+
+  const {Icon, title} = props
   return (
-    <div>
-      
+    <div className='flex flex-col items-center text-black cursor-pointer group w-12 sm:w-20 hover:text-white'>
+      <Icon className='h-12 mb-1 c-red group-hover:animate-bounce'/>
+      <p className='opacity-0 group-hover:opacity-100 tracking-widest'>{title}</p>
     </div>
   )
 }
